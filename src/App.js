73334df34f30e771b95915a0b452020c8da87cb5
Cyd Villavicencio <villacv@gmail.com>
2021-11-16 17:31:01 -0500

    added several new components and completed skeleton/ design of smart-brain

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
+import ImageLinkForm from './components/ImageLinkForm';
+// import FaceRecogition from './components/FaceRecognition'
+import Rank from './components/Rank';
+import Logo from './Logo/Logo'
+import { Component } from 'react';
+import Particles from 'react-particles-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
+
+class App extends Component {
+  constructor()  {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onSubmit = () => {
+    console.log('click')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles"
+                params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        {/* <FaceRecognition/> */}
+      </div>
+    )
+  } 
+  
 }
 
 export default App;
