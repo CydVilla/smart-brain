97e6e61454653c6933875484f241fc5fcb5ad900
Cyd Villavicencio <villacv@gmail.com>
2022-07-21 13:18:36 -0400

    url updates

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("https://shielded-headland-09246.herokuapp.com/imageurl", {
+    fetch("https://smartbrain-api1.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -88,7 +88,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://shielded-headland-09246.herokuapp.com/image", {
+          fetch("https://smartbrain-api1.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Register.js
+++ src/components/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://shielded-headland-09246.herokuapp.com/register', {
+    fetch('https://smartbrain-api1.herokuapp.com/register', {
       method: 'post',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
